a1d7919650d51bb1a4d0f28f4555a01a82e707be
huangjw27915 <huangjw27915@hundsun.com>
2021-02-07 14:40:08 +0800

    设计模式--创建型模式--原型模式

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
             * abstract_factory -- 抽象工厂模式
             * builder -- 生成器模式  
             * factory_method -- 工厂方法模式
+            * prototype -- 原型模式
         * structural -- 结构型模式
---
+++ src/design_patterns/creational/prototype/shapes/Shape.java
@@ -0,0 +1,36 @@
+package design_patterns.creational.prototype.shapes;
+
+import java.util.Objects;
+
+/**
+ * 通用形状接口
+ * @date 2021/2/7
+ */
+public abstract class Shape {
+    public int x;
+    public int y;
+    public String color;
+
+    public Shape() {
+    }
+
+    public Shape(Shape target) {
+        if (target != null) {
+            this.x = target.x;
+            this.y = target.y;
+            this.color = target.color;
+        }
+    }
+
+    @Override
+    public abstract Shape clone();
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Shape)) {
+            return false;
+        }
+        Shape shape2 = (Shape) object2;
+        return shape2.x == x && shape2.y == y && Objects.equals(shape2.color, color);
+    }
+}
